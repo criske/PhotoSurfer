3d38cbc0f2bf37dd14b71ba89c1a377b0ffb7b7c
cristianpela <cristypela05@gmail.com>
2018-09-19 15:03:35 +0300

    delete file main activity bad placement

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createCollection = functions.https.onRequest((request, response) => {
+    var message = {
+        data: {
+            id: '850'
+        },
+        topic: "COLLECTION_CREATED"
+    };
+    admin.messaging().send(message)
+        .then((response) => {
+            //response.status(200).send('Successfully sent message')
+            return 1
+        })
+        .catch((error) => {
+            // response.status(500).send('Error sending message: ' + error)
+            return 2
+        });
+    response.status(200).send('Successfully sent message')
+});
