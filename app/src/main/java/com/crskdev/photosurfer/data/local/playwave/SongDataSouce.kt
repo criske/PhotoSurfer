f38c4ec62977cf0f32feb2ea5ea2c9a3d2afc31d
cristianpela <cristypela05@gmail.com>
2018-10-14 14:44:17 +0300

    song title and artist sanitize

--- app/src/main/java/com/crskdev/photosurfer/data/local/playwave/SongDataSouce.kt
+++ app/src/main/java/com/crskdev/photosurfer/data/local/playwave/SongDataSouce.kt
@@ -30,9 +30,41 @@ class SongDataSource(contentResolver: ContentResolver) : ContentResolverDataSour
                     cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)),
                     cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)),
                     cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)))
+                    .sanitize()
 }
 
 
 data class Song(val id: Int,
                 val path: String, val title: String, val artist: String,
-                val duration: Int)
+                val duration: Int) {
+
+    companion object {
+        private const val UNKNOWN_ARTIST = "<unknown>"
+        private const val MANGLED_TITLE_DELIM = "-"
+        private val urlRegex by lazy {
+            "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]\\.[^\\s]{2,})"
+                    .toRegex()
+        }
+    }
+
+    fun sanitize(): Song {
+        val (title: String, artist: String) = if (artist == UNKNOWN_ARTIST) {
+            this.title.split(MANGLED_TITLE_DELIM)
+                    .takeIf { it.size == 2 }
+                    ?.let { it[1] to it[0] }
+                    ?: this.title to this.artist
+        } else this.title to this.artist
+        val title1 = title.trim()
+                .replaceFirst(urlRegex, "")
+                .split(" ").joinToString(" "){
+                    it.trim().capitalize()
+                }
+        val artist1 = artist.trim()
+                .replaceFirst(urlRegex, "")
+                .split(" ").joinToString(" ") {
+                    it.trim().capitalize()
+                }
+        return copy(title = title1, artist = artist1)
+    }
+
+}
---
+++ app/src/test/java/com/crskdev/photosurfer/data/local/playwave/SongTest.kt
@@ -0,0 +1,43 @@
+@file:Suppress("NestedLambdaShadowedImplicitParameter")
+
+package com.crskdev.photosurfer.data.local.playwave
+
+import org.junit.Test
+
+/**
+ * Created by Cristian Pela on 14.10.2018.
+ */
+class SongTest {
+
+    @Test
+    fun sanitize() {
+        """
+            Song(id=20878, path=/storage/emulated/0/Download/Kick Bong - Landscape (Cydelix remix) _ Cosmicleaf.com-vbVMJy5Uewo.mp3, title=Kick Bong - Landscape (Cydelix remix) _ Cosmicleaf.com-vbVMJy5Uewo, artist=<unknown>, duration=289104)
+            Song(id=20876, path=/storage/emulated/0/Download/Electribe 101 - Talking With Myself 98 (Canny remix).mp3, title=Electribe 101 - Talking With Myself 98 (Canny remix), artist=<unknown>, duration=428591)
+            Song(id=20872, path=/storage/emulated/0/Download/Hamatsuki - You As Ronnie.mp3, title=Hamatsuki - You As Ronnie, artist=<unknown>, duration=405984)
+            Song(id=20870, path=/storage/emulated/0/Download/Quadrophenia - Paradise.mp3, title=Quadrophenia - Paradise, artist=<unknown>, duration=278184)
+            Song(id=20575, path=/storage/emulated/0/Download/The Starseeds - Heavensairportcoffeeshoprestaurant.mp3, title=The Starseeds - Heavensairportcoffeeshoprestaurant, artist=<unknown>, duration=406872)
+            Song(id=3427, path=/storage/emulated/0/Music/music/Étienne De Crécy - When Jack Met Jill.mp3, title=Étienne De Crécy - When Jack Met Jill, artist=<unknown>, duration=335595)
+            Song(id=3426, path=/storage/emulated/0/Music/music/[www.fisierulmeu.ro] alexandra ungureanu - lumea viseaza.mp3, title=[www.fisierulmeu.ro] alexandra ungureanu - lumea viseaza, artist=<unknown>, duration=236121)
+            Song(id=3425, path=/storage/emulated/0/Music/music/[CHILLHOUSE] Caia-Heavy Weather [Full].mp3, title=[CHILLHOUSE] Caia-Heavy Weather [Full], artist=<unknown>, duration=308349)
+            Song(id=3424, path=/storage/emulated/0/Music/music/zzzz_momentscpad.mp3, title=zzzz_momentscpad, artist=<unknown>, duration=384287,)
+            Song(id=3423, path=/storage/emulated/0/Music/music/zLusine - Push.mp3, title=zLusine - Push, artist=<unknown>, duration=328882,)
+        """.trimIndent().split("\n").forEach { println(songFromString(it).sanitize()) }
+    }
+
+
+    private fun songFromString(s: String): Song {
+        fun extract(mark: String) =
+                s.indexOf(mark).let {
+                    val end = s.indexOf(",", it).takeIf { it != -1 } ?: s.indexOf(")", it)
+                    s.substring(it + mark.length, end)
+                }
+        val id = extract("id=").toInt()
+        val path = extract("path=")
+        val title = extract("title=")
+        val author = extract("artist=")
+        val duration = extract("duration=").toInt()
+        return Song(id, path, title, author, duration)
+    }
+
+}
